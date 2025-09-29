2ad260f08db9a35ae57986d1951b015a43251ee7
Heitor <heitorbolisw4@gmail.com>
2025-09-29 12:09:33 -0300

    Subindo desafio de código e alguns livros

---
+++ Desafio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira o token de acesso
+        Console.WriteLine("Digite sua credencial abaixo");
+        string? token = Console.ReadLine();
+
+        if (token?.Length != 10 || token?[0] != 'A' || !token.Any(char.IsDigit))
+        {
+            Console.WriteLine("Acesso Negado");
+        }
+        else
+        {
+            Console.WriteLine("Acesso permitido");            
+        }
+
+    }
+}
